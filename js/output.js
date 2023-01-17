cdbbf984358c8ebb5b5e217264bf301352b5d810
smartinfologiks <devops@smartinfologiks.com>
2023-01-18 01:20:20 +0530

    Default loading of jquery and ajax framework for developer easiness

--- js/output.js
+++ js/output.js
@@ -1,1 +1,20 @@
 parent.waitingDialog.hide();
+
+//Use CDN to load JS Libraries
+function lgksLoadJS(scriptLib, callback) {
+    if(Array.isArray(scriptLib)) {
+        var counter = 0;
+        scriptLib.forEach(function(src) {
+            $.getScript("https://unpkg.com/"+src, function(data, textStatus, jqxhr) {
+                counter++;
+                if(counter>=scriptLib.length) {
+                    if(typeof callback == "function") callback(textStatus, data);
+                }
+            })
+        });
+    } else {
+        $.getScript("https://unpkg.com/"+scriptLib, function(data, textStatus, jqxhr) {
+            if(typeof callback == "function") callback(textStatus, data);
+        })
+    }
+}
